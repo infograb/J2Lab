ba1d5a75ff4d6f4ed739cc88d2e78235b35258a1
jeff <jeff@infograb.net>
2023-09-05 20:31:04 +0900

    feat: epic에 이미지 올리는 기능 추가

--- internal/j2g/text.go
+++ internal/j2g/text.go
@@ -5,38 +5,35 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
-	gitlab "github.com/xanzy/go-gitlab"
 	"gitlab.com/infograb/team/devops/toy/j2lab/internal/adf"
 	"gitlab.com/infograb/team/devops/toy/j2lab/internal/config"
 	"gitlab.com/infograb/team/devops/toy/j2lab/internal/jirax"
 )
 
 // comment -> comments : GitLab 작성자는 API owner이지만, 텍스트로 Jira 작성자를 표현
-func convertToIssueNoteOptions(issueKey string, jiraComment *jirax.Comment, mediaMarkdown []*adf.Media, userMap UserMap, isProject bool) (*gitlab.CreateIssueNoteOptions, error) {
+func formatNote(issueKey string, jiraComment *jirax.Comment, mediaMarkdown []*adf.Media, userMap UserMap, isProject bool) (*string, *time.Time, error) {
 	created, err := time.Parse("2006-01-02T15:04:05.000-0700", jiraComment.Created)
 	if err != nil {
-		return nil, errors.Wrap(err, "Error parsing time")
+		return nil, nil, errors.Wrap(err, "Error parsing time")
 	}
 
 	cfg, err := config.GetConfig()
 	if err != nil {
-		return nil, errors.Wrap(err, "Error getting config")
+		return nil, nil, errors.Wrap(err, "Error getting config")
 	}
 
 	commentLink := fmt.Sprintf("%s/browse/%s?focusedCommentId=%s", cfg.Jira.Host, issueKey, jiraComment.ID)
 	dateFormat := fmt.Sprintf("%s at %s", created.Format("January 02, 2006"), created.Format("3:04 PM"))
-	formatedBody, err := formatDescription(issueKey, jiraComment.Body, mediaMarkdown, userMap, isProject)
+
+	adfBlock := jiraComment.Body.Content
+	markdownBody, err := adf.AdfToGitLabMarkdown(adfBlock, mediaMarkdown, adf.UserMap(userMap), isProject)
 	if err != nil {
-		return nil, errors.Wrap(err, "Error formatting description")
+		return nil, nil, errors.Wrap(err, "Error converting ADF to GitLab Markdown")
 	}
 
-	body := fmt.Sprintf("%s\n\n%s by %s [[Original](%s)]",
-		*formatedBody, dateFormat, jiraComment.Author.DisplayName, commentLink)
-
-	return &gitlab.CreateIssueNoteOptions{
-		Body:      &body,
-		CreatedAt: &created,
-	}, nil
+	result := fmt.Sprintf("%s\n\n%s by %s [[Original](%s)]",
+		markdownBody, dateFormat, jiraComment.Author.DisplayName, commentLink)
+	return &result, &created, nil
 }
 
 func formatDescription(issueKey string, content *adf.ADF, mediaMarkdown []*adf.Media, userMap UserMap, isProject bool) (*string, error) {
