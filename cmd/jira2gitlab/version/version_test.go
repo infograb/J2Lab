c9314083868312061f7d44cbf1c29dd13a5f5c9f
kane <kane@MacBook-Pro.local>
2023-09-09 20:00:02 +0900

    fix: license

--- cmd/jira2gitlab/version/version_test.go
+++ cmd/jira2gitlab/version/version_test.go
@@ -1,3 +1,14 @@
+/*
+ * This file is part of the InfoGrab project.
+ *
+ * Copyright (C) 2023 InfoGrab
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * it is available under the terms of the GNU Lesser General Public License
+ * by the Free Software Foundation, either version 3 of the License or by the Free Software Foundation
+ * (at your option) any later version.
+ */
+
 package version
 
 import (
